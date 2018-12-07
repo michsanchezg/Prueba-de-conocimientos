d369058c6fa1cad15494d84eedb5b542d1bea887
Michelle Sánchez Guerrero <45677223+michsanchezg@users.noreply.github.com>
2018-12-06 22:19:03 -0600

    Add files via upload

---
+++ Ejercicio1_RESTAPI_POST.py
@@ -0,0 +1,37 @@
+#Michelle Sánchez Guerrero
+#Ejercicio 1. Tipo POST
+
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route('/palindromo', methods= ['POST'])
+def postear():
+    listaBooleanos = []
+
+    data = request.get_json()
+
+    listaPalabras = data["palabras"]
+
+    for palabra in listaPalabras:
+
+        for char in palabra:
+            if not char.isalnum():
+                palabra = palabra.replace(char, "")
+
+        palabra = palabra.lower()
+        palabra = palabra.replace(" ", "")
+        palabraInvertida = palabra[::-1]
+        if palabra == palabraInvertida:
+            listaBooleanos.append(True)
+        else:
+            listaBooleanos.append(False)
+
+    resultado = json.dumps({"palindromos" : listaBooleanos}, indent=4)
+
+    return resultado
+
+
+if __name__ == '__main__':
+    app.run()
